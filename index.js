7b539ed129588c5d186223eba7c0ff7811524bd3
Joel Lavaisse <joel.lavaisse@gmail.com>
2020-12-04 18:06:22 -0300

    Agregado de rutas

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ const port = process.env.PORT || 3000;
 //for our static assets
 app.use('/cv-online', express.static(path.join(__dirname, '01 - CV online')));
 app.use('/home-banking', express.static(path.join(__dirname, '02 - Home banking')));
+app.use('/rompecabezas', express.static(path.join(__dirname, '03 - Rompecabezas')));
+app.use('/pixel-art', express.static(path.join(__dirname, '04 - Pixel art')));
+app.use('/ciudad-zombie', express.static(path.join(__dirname, '05 - Ciudad zombie')));
+app.use('/reservando', express.static(path.join(__dirname, '06 - Reservando')));
+app.use('/encuestados', express.static(path.join(__dirname, '07 - Encuestados')));
+app.use('/mapa-interactivo', express.static(path.join(__dirname, '08 - Mapa interactivo')));
+app.use('/que-veo-hoy', express.static(path.join(__dirname, '09 - Que veo hoy')));
+app.use('/peli-vs-peli', express.static(path.join(__dirname, '10 - Peli vs peli')));
 app.use('/', (req, res) => res.send('aca toy!'));
 
 // Listen for requests
